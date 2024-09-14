cbdc28bc86f6156f7050b7dd0f7b332120bfef51
AndrewBerdyshev <an.berdyshev@gmail.com>
2024-09-14 07:44:28 +0700

    last2taskcommit

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,1 +1,1 @@
-cat $(find . -type f -name "foo.txt" -print; find find . -type f -name "bar.txt" -print; find . -type f -name "baz.txt" -print)
+cat $(find . -type f -name "foo.txt" -print; find . -type f -name "bar.txt" -print; find . -type f -name "baz.txt" -print)
